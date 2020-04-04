dfbfdc34e3af8fdacd62e594063c59bf0842ceb8
Will Sonnex <william.sonnex@qlearsite.com>
2020-04-04 10:26:02 -0700

    work

--- workpackage-manager/src/track/wpm/app.py
+++ workpackage-manager/src/track/wpm/app.py
@@ -1,18 +1,27 @@
 from flask import Flask, json, request
 import logging
 import yaml
-from typing import NamedTuple
+from typing import NamedTuple, Dict, List, Iterator
+import math
 
-from track.wpm.open_project import OpenProjectClient
+from track.wpm.open_project import OpenProjectClient, WorkPackageSpec
 
 CONFIG_FILE = 'config.yml'
 SUCCESS_RESPONSE = json.dumps({'success': True})
+ORDER_TYPE_ID = 1
+PRODUCTION_ORDER_TYPE_ID = 4
+CONFIRMED_STATUS_ID = 4
 
 
-class ProductionField(NamedTuple):
+class BatchedField(NamedTuple):
     name: str
-    openProjectKey: str
-    batchSize: int
+    open_project_key: str
+    batch_size: int
+
+
+class Order(NamedTuple):
+    subject: str
+    quantities: Dict[str, int]
 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -20,22 +29,69 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(level
 with open(CONFIG_FILE, 'r') as f:
     config = yaml.safe_load(f)
 
-op_client = OpenProjectClient(config)
+openproject = OpenProjectClient(config)
 
-production_fields = {d['name']: ProductionField(**d) for d in config['productionFields']}
+batched_fields = [BatchedField(**d) for d in config['fields']['batched']]
 
-api = Flask(__name__)
+app = Flask(__name__)
 
 
-@api.route('/update', methods=['POST'])
+def create_batched_sub_orders(super_order: Order, existing_sub_orders: List[Order]) -> Iterator[Order]:
+    for batched_field in batched_fields:
+        key = batched_field.open_project_key
+        batch_size = batched_field.batch_size
+        required = super_order.quantities[key]
+        existing = sum([existing.quantities[key] for existing in existing_sub_orders])
+        new_batches = math.ceil(max(0, existing - required) / batched_field.batch_size)
+        subject = '{} - {} x{}'.format(super_order.subject, batched_field.name, batch_size)
+        for _ in range(new_batches):
+            yield Order(quantities={key: batch_size}, subject=subject)
+
+
+def parse_order(order_dict) -> Order:
+    quantities = {}
+    for batched_field in batched_fields:
+        key = batched_field.open_project_key
+        quantities[key] = order_dict[key]
+    return Order(quantities=quantities, subject=order_dict['subject'])
+
+
+def create_production_order(parent_id: int, order: Order):
+    spec = WorkPackageSpec(
+        subject=order.subject,
+        type_id=PRODUCTION_ORDER_TYPE_ID,
+        parent_id=parent_id,
+        extra_fields=order.quantities
+    )
+    return openproject.create_work_package(spec)
+
+
+def update_work_package(work_package_id):
+    logging.info('Updating work package id {}'.format(work_package_id))
+    wp = openproject.get_work_package(work_package_id)
+    linked_fields = wp['_embedded']
+    if linked_fields['type']['id'] == ORDER_TYPE_ID and linked_fields['status']['id'] == CONFIRMED_STATUS_ID:
+        wp_order = parse_order(wp)
+        logging.info('{} is a confirmed order: {}'.format(work_package_id, wp_order))
+        existing_children = openproject.get_children(wp)
+        existing_orders = [parse_order(child) for child in existing_children]
+        logging.info('found existing batches: {}'.format(existing_orders))
+        new_batches = create_batched_sub_orders(wp_order, existing_orders)
+        logging.info('new batches required: {}'.format(new_batches))
+        for order in new_batches:
+            create_production_order(work_package_id, order)
+
+
+@app.route('/update', methods=['POST'])
 def update():
     req_json = request.get_json()
-    logging.info(req_json)
     if 'work_package' in req_json:
         wp_id = int(req_json['work_package']['id'])
-        logging.info(op_client.work_package(wp_id))
+        update_work_package(wp_id)
+    else:
+        logging.warning('unrecognised request {}'.format(json.dumps(req_json)))
     return SUCCESS_RESPONSE
 
 
 if __name__ == '__main__':
-    api.run(host='0.0.0.0', port=config['server']['port'])
+    app.run(host='0.0.0.0', port=config['server']['port'])
--- workpackage-manager/src/track/wpm/open_project.py
+++ workpackage-manager/src/track/wpm/open_project.py
@@ -1,5 +1,33 @@
+import logging
+from typing import NamedTuple, Dict, Any, Optional
+
 import requests
 from requests.auth import HTTPBasicAuth
+
+
+class WorkPackageSpec(NamedTuple):
+    type_id: int
+    subject: str
+    extra_fields: Dict[str, Any] = {}
+    status_id: Optional[int] = None
+    parent_id: Optional[int] = None
+
+    def as_openproject_object(self):
+        return {
+            'subject': self.subject,
+            '_links': [{
+                'status': {
+                    'href': '/api/v3/statuses/{}'.format(self.status_id) if self.status_id else None
+                },
+                'type': {
+                    'href': '/api/v3/types/{}'.format(self.type_id)
+                },
+                'parent': {
+                    'href': '/api/v3/work_packages/{}'.format(self.parent_id) if self.parent_id else None
+                }
+            }],
+            **self.extra_fields
+        }
 
 
 class OpenProjectClient:
@@ -7,13 +35,31 @@ class OpenProjectClient:
     api_url: str
 
     def __init__(self, config):
-        self.api_url = config['openProjectApi']['url'] + '/api/v3'
-
-        api_key = config['openProjectApi']['key']
+        self.api_url = config['open_project_api']['url'] + '/api/v3'
+        api_key = config['open_project_api']['key']
         self.auth = HTTPBasicAuth(username='apikey', password=api_key)
 
     def get(self, path: str):
-        return requests.get('{}{}'.format(self.api_url, path), auth=self.auth)
+        url = '{}{}'.format(self.api_url, path)
+        logging.info('GET {}'.format(url))
+        result = requests.get(url, auth=self.auth).json()
+        logging.info('response {}'.format(result))
+        return result
 
-    def work_package(self, id: int):
+    def post(self, path: str, json):
+        url = '{}{}'.format(self.api_url, path)
+        logging.info('POST {} with {}'.format(url, json))
+        return None
+        # result = requests.post(url=url, json=json, auth=self.auth).json()
+        # logging.info('response {}'.format(result))
+        # return result
+
+    def get_work_package(self, id: int):
         return self.get('/work_packages/{}'.format(id)).json()
+
+    def get_children(self, work_package):
+        children = work_package['_links']['children']
+        return [self.get(child['href']) for child in children]
+
+    def create_work_package(self, spec: WorkPackageSpec):
+        return self.post('/work_packages', spec.as_openproject_object())
