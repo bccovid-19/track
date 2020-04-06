091b7768280720109d3cb3a47f19458615855209
Will Sonnex <william.sonnex@qlearsite.com>
2020-04-05 21:25:59 -0700

    meh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-docker-compose -f apps/workpackage-manager/docker-compose.yml build
 docker-compose -f apps/templating/docker-compose.yml build
 docker-compose -f apps/templating/docker-compose.yml up
